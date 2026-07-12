6afd5eaa5a65ea3c79a2784956e6fb7cb88dea50
Sahil Vishwakarma <sahilv0246@gmail.com>
2026-07-12 14:10:52 +0530

    Add Axios instance with interceptors for API requests
    Create an Axios instance with request and response interceptors for API calls.

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('af_token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response && err.response.status === 401) {
+      localStorage.removeItem('af_token');
+      localStorage.removeItem('af_user');
+      if (window.location.pathname !== '/login') window.location.href = '/login';
+    }
+    return Promise.reject(err);
+  }
+);
+
+export default api;
